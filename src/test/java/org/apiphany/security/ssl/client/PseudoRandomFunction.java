d8b511826a77cd8cfd01cbcf8e727bfd58887f3a
raduking <radu.lazin@boatyardx.com>
2025-08-04 23:56:29 +0300

    Added `PRFLabel` enum.

--- src/test/java/org/apiphany/security/ssl/client/PseudoRandomFunction.java
+++ src/test/java/org/apiphany/security/ssl/client/PseudoRandomFunction.java
@@ -11,12 +11,20 @@ public class PseudoRandomFunction {
 
 	public static final String ALGORITHM_SHA384 = "HmacSHA384";
 
-	public static byte[] apply(final byte[] secret, final String label, final byte[] seed, final int length) throws Exception {
+	public static byte[] apply(final byte[] secret, final PRFLabel label, final byte[] seed, final int length) throws Exception {
 		return apply(secret, label, seed, length, ALGORITHM_SHA384);
 	}
 
-	public static byte[] apply(final byte[] secret, final String label, final byte[] seed, final int length, final String algorithm) throws Exception {
-		byte[] labelBytes = label.getBytes(StandardCharsets.US_ASCII);
+	public static byte[] apply(final byte[] secret, final String label, final byte[] seed, final int length) throws Exception {
+		return apply(secret, label.getBytes(StandardCharsets.US_ASCII), seed, length, ALGORITHM_SHA384);
+	}
+
+	public static byte[] apply(final byte[] secret, final PRFLabel label, final byte[] seed, final int length, final String algorithm) throws Exception {
+		return apply(secret, label.toByteArray(), seed, length, algorithm);
+	}
+
+	public static byte[] apply(final byte[] secret, final byte[] label, final byte[] seed, final int length, final String algorithm) throws Exception {
+		byte[] labelBytes = label;
 		byte[] labelSeed = new byte[labelBytes.length + seed.length];
 		System.arraycopy(labelBytes, 0, labelSeed, 0, labelBytes.length);
 		System.arraycopy(seed, 0, labelSeed, labelBytes.length, seed.length);
@@ -30,8 +38,8 @@ public class PseudoRandomFunction {
 
 		byte[] a = seed;
 		byte[] result = new byte[length];
-		int offset = 0;
 
+		int offset = 0;
 		while (offset < length) {
 			a = mac.doFinal(a); // a(i) = HMAC(secret, a(i-1))
 			byte[] output = mac.doFinal(Bytes.concatenate(a, seed));
@@ -39,7 +47,6 @@ public class PseudoRandomFunction {
 			System.arraycopy(output, 0, result, offset, copyLength);
 			offset += copyLength;
 		}
-
 		return result;
 	}
 
