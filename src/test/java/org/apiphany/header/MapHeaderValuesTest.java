8450090e28d6e1f00061ad78706dc03935e6bee2
Radu Sebastian LAZIN <raduking@gmail.com>
2025-08-23 15:11:50 +0300

    More test coverage.

--- src/test/java/org/apiphany/header/MapHeaderValuesTest.java
+++ src/test/java/org/apiphany/header/MapHeaderValuesTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +20,7 @@ import org.junit.jupiter.api.Test;
  */
 class MapHeaderValuesTest {
 
+	private static final String HEADER_NAME = "headerName";
 	private static final String HEADER_VALUE = "headerValue";
 
 	@Test
@@ -37,4 +41,32 @@ class MapHeaderValuesTest {
 		assertThat(headerValues, hasSize(0));
 	}
 
+	@Test
+	void shouldThrowExceptionOnNextForSimpleInstance() {
+		MapHeaderValues mhv = new MapHeaderValues();
+		IllegalStateException e = assertThrows(IllegalStateException.class, mhv::getNext);
+
+		assertThat(e.getMessage(), equalTo("Cannot get header values, end of the chain reached."));
+	}
+
+	@Test
+	void shouldThrowExceptionOnGetForSimpleInstanceAndNonMaps() {
+		MapHeaderValues mhv = new MapHeaderValues();
+		IllegalStateException e = assertThrows(IllegalStateException.class, () -> mhv.get(HEADER_NAME, mhv));
+
+		assertThat(e.getMessage(), equalTo("Cannot get header values, end of the chain reached."));
+	}
+
+	@Test
+	void shouldReturnHeaderValueFromNextInChain() {
+		MapHeaderValues mhv = new MapHeaderValues();
+		HeaderValues next = mock(HeaderValues.class);
+		doReturn(List.of(HEADER_VALUE)).when(next).get(HEADER_NAME, mhv);
+		mhv.setNext(next);
+
+		List<String> result = mhv.get(HEADER_NAME, mhv);
+
+		assertThat(result, hasSize(1));
+		assertThat(result.getFirst(), equalTo(HEADER_VALUE));
+	}
 }
