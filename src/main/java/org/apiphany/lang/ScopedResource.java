11e9242921adc1bd3f4e9583cff03497443d8310
raduking <radu.lazin@boatyardx.com>
2025-08-27 11:25:46 +0300

    Add resource checking method to `ScopedResource`.

--- src/main/java/org/apiphany/lang/ScopedResource.java
+++ src/main/java/org/apiphany/lang/ScopedResource.java
@@ -4,12 +4,12 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * A wrapper for managing the life-cycle of AutoCloseable resources with scope control.
+ * A wrapper for managing the life-cycle of {@link AutoCloseable} resources with scope control.
  * <p>
  * This class provides controlled access to resources while ensuring proper cleanup based on the managed flag. It
  * supports both managed (automatically closed) and unmanaged (manually closed) resource patterns.
  *
- * @param <T> the type of the AutoCloseable resource being wrapped
+ * @param <T> the type of the {@link AutoCloseable} resource being wrapped
  *
  * @author Radu Sebastian LAZIN
  */
@@ -120,4 +120,29 @@ public class ScopedResource<T extends AutoCloseable> {
 	public static <T extends AutoCloseable> ScopedResource<T> unmanaged(final T resource) {
 		return of(resource, false);
 	}
+
+	/**
+	 * Checks the first parameter against the second for the same underlying reference. If the referenced resources are the
+	 * same and they are both managed then we return an unmanaged scoped resource. Only one scoped resource should manage
+	 * the same resource.
+	 *
+	 * @param <T> the type of the {@link AutoCloseable} resources being wrapped
+	 *
+	 * @param checkedResource the resource to check
+	 * @param resource the resource to check against
+	 * @return a checked scoped resource
+	 */
+	@SuppressWarnings("resource")
+	public static <T extends AutoCloseable> ScopedResource<T> checked(
+			final ScopedResource<T> checkedResource,
+			final ScopedResource<T> resource) {
+		if (checkedResource.isNotManaged() || resource.isNotManaged()) {
+			return checkedResource;
+		}
+		T rawCheckedResource = checkedResource.unwrap();
+		return rawCheckedResource == resource.unwrap()
+				? ScopedResource.unmanaged(rawCheckedResource)
+				: checkedResource;
+	}
+
 }
