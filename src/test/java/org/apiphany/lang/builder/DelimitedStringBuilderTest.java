32b88dda2e31f4e944df06fd7d83f6015fcb83a5
raduking <radu.lazin@boatyardx.com>
2025-08-28 11:29:07 +0300

    More test coverage.

---
+++ src/test/java/org/apiphany/lang/builder/DelimitedStringBuilderTest.java
@@ -0,0 +1,53 @@
+package org.apiphany.lang.builder;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for {@link DelimitedStringBuilder}
+ *
+ * @author Radu Sebastian LAZIN
+ */
+class DelimitedStringBuilderTest {
+
+	private static final String DELIMITER = "666";
+
+	@Test
+	void shouldBuildADelimitedStringWithMultiplePaths() {
+		String result = DelimitedStringBuilder.builder(DELIMITER)
+				.path("xxx", "yyy", "zzz")
+				.build();
+
+		assertThat(result, equalTo("xxx" + DELIMITER + "yyy" + DELIMITER + "zzz"));
+	}
+
+	@Test
+	void shouldBuildADelimitedStringWithMultiplePathsAsSuffix() {
+		String result = DelimitedStringBuilder.builder(DELIMITER)
+				.asSuffix()
+				.path("xxx", "yyy", "zzz")
+				.build();
+
+		assertThat(result, equalTo(DELIMITER + "xxx" + DELIMITER + "yyy" + DELIMITER + "zzz"));
+	}
+
+	@Test
+	void shouldThrowExceptionIfPathIsNull() {
+		DelimitedStringBuilder dsb = DelimitedStringBuilder.builder(DELIMITER);
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> dsb.path((String) null));
+
+		assertThat(e.getMessage(), equalTo("Parameter path should not be null"));
+	}
+
+	@Test
+	void shouldThrowExceptionIfPathsIsNull() {
+		DelimitedStringBuilder dsb = DelimitedStringBuilder.builder(DELIMITER);
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> dsb.path((String[]) null));
+
+		assertThat(e.getMessage(), equalTo("Parameter paths should not be null"));
+	}
+
+}
