0729e680934c9dacf1855460bd6568dac51c5726
Radu Sebastian LAZIN <raduking@gmail.com>
2025-09-15 03:52:10 +0300

    Added creation of token providers based on OAuth2 properties.

--- src/main/java/org/apiphany/security/oauth2/OAuth2Registry.java
+++ src/main/java/org/apiphany/security/oauth2/OAuth2Registry.java
@@ -3,9 +3,14 @@ package org.apiphany.security.oauth2;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.BiFunction;
 
 import org.apiphany.lang.collections.Maps;
+import org.apiphany.security.AuthenticationTokenProvider;
 
 /**
  * OAuth2 registry which holds all the registrations and providers defined by {@link OAuth2Properties}.
@@ -62,5 +67,50 @@ public class OAuth2Registry {
 	 */
 	public Collection<OAuth2ResolvedRegistration> entries() {
 		return entries.values();
+	}
+
+	/**
+	 * Returns a new OAuth2 token provider based on the given parameters.
+	 *
+	 * @param clientRegistrationName the client registration name
+	 * @param configuration the OAuth2 token provider configuration
+	 * @param tokenRefreshScheduler the token refresh scheduler
+	 * @param tokenClientSupplier the supplier for the client that will make the actual token requests
+	 * @return a new OAuth2 token provider
+	 */
+	public OAuth2TokenProvider tokenProvider(
+			final String clientRegistrationName,
+			final OAuth2TokenProviderConfiguration configuration,
+			final ScheduledExecutorService tokenRefreshScheduler,
+			final BiFunction<OAuth2ClientRegistration, OAuth2ProviderDetails, AuthenticationTokenProvider> tokenClientSupplier) {
+		OAuth2ResolvedRegistration registration = get(clientRegistrationName);
+		return new OAuth2TokenProvider(configuration, registration, tokenRefreshScheduler, tokenClientSupplier);
+	}
+
+	/**
+	 * Returns a new OAuth2 token provider based on the given parameters.
+	 *
+	 * @param clientRegistrationName the client registration name
+	 * @param tokenClientSupplier the supplier for the client that will make the actual token requests
+	 * @return a new OAuth2 token provider
+	 */
+	@SuppressWarnings("resource")
+	public OAuth2TokenProvider tokenProvider(
+			final String clientRegistrationName,
+			final BiFunction<OAuth2ClientRegistration, OAuth2ProviderDetails, AuthenticationTokenProvider> tokenClientSupplier) {
+		OAuth2TokenProviderConfiguration configuration = OAuth2TokenProviderConfiguration.defaults();
+		ScheduledExecutorService tokenRefreshScheduler = Executors.newScheduledThreadPool(0, Thread.ofVirtual().factory());
+		return tokenProvider(clientRegistrationName, configuration, tokenRefreshScheduler, tokenClientSupplier);
+	}
+
+	/**
+	 * Returns a list of OAuth2 token providers based on this registry.
+	 *
+	 * @param tokenClientSupplier the supplier for the client that will make the actual token requests
+	 * @return a list of OAuth2 token providers based on this registry
+	 */
+	public List<OAuth2TokenProvider> tokenProviders(
+			final BiFunction<OAuth2ClientRegistration, OAuth2ProviderDetails, AuthenticationTokenProvider> tokenClientSupplier) {
+		return entries.keySet().stream().map(key -> tokenProvider(key, tokenClientSupplier)).toList();
 	}
 }
