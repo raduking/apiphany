f28ca8d92af794a70c0da72fc7e1939e1fde92f9
raduking <radu.lazin@boatyardx.com>
2026-01-08 19:57:55 +0200

    More test coverage.

--- src/test/java/org/apiphany/http/ContentEncodingTest.java
+++ src/test/java/org/apiphany/http/ContentEncodingTest.java
@@ -4,8 +4,11 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * Test class for {@link ContentEncoding}.
@@ -28,5 +31,41 @@ class ContentEncodingTest {
 		String stringValue = contentEncoding.value().toUpperCase();
 		boolean result = contentEncoding.matches(stringValue);
 		assertTrue(result);
+	}
+
+	@ParameterizedTest
+	@EnumSource(ContentEncoding.class)
+	void shouldMatchValidValueEvenIfMixedCase(final ContentEncoding contentEncoding) {
+		String stringValue = switch (contentEncoding) {
+			case GZIP -> "gZiP";
+			case DEFLATE -> "dEfLaTe";
+			case BR -> "bR";
+			default -> contentEncoding.value();
+		};
+		boolean result = contentEncoding.matches(stringValue);
+		assertTrue(result);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideValuesForParsingFromList")
+	void shouldParseValueFromAList(final List<String> values, final ContentEncoding expectedEncoding) {
+		ContentEncoding result = ContentEncoding.parse(values);
+
+		assertThat(result, equalTo(expectedEncoding));
+	}
+
+	private static Object[][] provideValuesForParsingFromList() {
+		return new Object[][] {
+				{ List.of("gzip"), ContentEncoding.GZIP },
+				{ List.of("deflate"), ContentEncoding.DEFLATE },
+				{ List.of("br"), ContentEncoding.BR },
+				{ List.of("gzip", "deflate"), ContentEncoding.GZIP },
+				{ List.of("deflate", "br"), ContentEncoding.DEFLATE },
+				{ List.of("br", "gzip"), ContentEncoding.BR },
+				{ List.of("unknown", "gzip"), ContentEncoding.GZIP },
+				{ List.of("unknown1", "unknown2"), null },
+				{ List.of(), null },
+				{ null, null }
+		};
 	}
 }
