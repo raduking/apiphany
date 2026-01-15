c32f716f66223cffa4eb0c2beb85f226d95223e9
raduking <radu.lazin@boatyardx.com>
2026-01-15 03:09:45 +0200

    Updated `ParameterFunction` to use `Parameter.value`.

--- src/test/java/org/apiphany/ParameterTest.java
+++ src/test/java/org/apiphany/ParameterTest.java
@@ -4,7 +4,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,55 +17,97 @@ import org.junit.jupiter.api.Test;
  */
 class ParameterTest {
 
+	private static final String NAME = "name";
+	private static final String VALUE = "value";
+	private static final String BLANK_STRING = "   ";
+
 	@Test
 	void shouldBuildParameterObjectFromStringValues() {
-		Parameter parameter = Parameter.of("name", "value");
+		Parameter parameter = Parameter.of(NAME, VALUE);
 
-		assertThat(parameter.name(), equalTo("name"));
-		assertThat(parameter.value(), equalTo("value"));
+		assertThat(parameter.name(), equalTo(NAME));
+		assertThat(parameter.value(), equalTo(VALUE));
 	}
 
 	@Test
 	void shouldBuildParameterObjectFromObjectValues() {
-		Parameter parameter = Parameter.of("name", 123);
+		Parameter parameter = Parameter.of(NAME, 123);
 
-		assertThat(parameter.name(), equalTo("name"));
+		assertThat(parameter.name(), equalTo(NAME));
 		assertThat(parameter.value(), equalTo("123"));
 	}
 
 	@Test
 	void shouldBuildParameterObjectFromListValues() {
-		Parameter parameter = Parameter.of("name", List.of(1, 2, 3));
+		Parameter parameter = Parameter.of(NAME, List.of(1, 2, 3));
 
-		assertThat(parameter.name(), equalTo("name"));
+		assertThat(parameter.name(), equalTo(NAME));
 		assertThat(parameter.value(), equalTo("1,2,3"));
 	}
 
 	@Test
 	void shouldBuildParameterObjectFromArrayValues() {
-		Parameter parameter = Parameter.of("name", new Object[] { "a", "b", "c" });
+		Parameter parameter = Parameter.of(NAME, new Object[] { "a", "b", "c" });
 
-		assertThat(parameter.name(), equalTo("name"));
+		assertThat(parameter.name(), equalTo(NAME));
 		assertThat(parameter.value(), equalTo("a,b,c"));
 	}
 
 	@Test
-	void shouldThrowExceptionWhenBuildingParameterWithNullName() {
-		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> Parameter.of(null, "value"));
+	void shouldBuildParameterObjectFromStringArrayValues() {
+		Parameter parameter = Parameter.of(NAME, new String[] { "a", "b", "c" });
 
-		assertThat(e.getMessage(), equalTo("Parameter name cannot be null or blank"));
+		assertThat(parameter.name(), equalTo(NAME));
+		assertThat(parameter.value(), equalTo("a,b,c"));
+	}
+
+	static record A(String name) {
+
+		@Override
+		public final String toString() {
+			return name;
+		}
+	}
+
+	@Test
+	void shouldBuildParameterObjectFromObject() {
+		Parameter parameter = Parameter.of(NAME, new A("test"));
+
+		assertThat(parameter.name(), equalTo(NAME));
+		assertThat(parameter.value(), equalTo("test"));
+	}
+
+	@Test
+	void shouldThrowExceptionWhenBuildingParameterWithConstructorAndNullName() {
+		NullPointerException e = assertThrows(NullPointerException.class, () -> new Parameter(null, VALUE));
+
+		assertThat(e.getMessage(), equalTo("parameter name cannot be null"));
+	}
+
+	@Test
+	void shouldThrowExceptionWhenBuildingParameterWithConstructorAndBlankName() {
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Parameter(BLANK_STRING, VALUE));
+
+		assertThat(e.getMessage(), equalTo("Parameter name cannot be blank"));
+	}
+
+	@Test
+	void shouldThrowExceptionWhenBuildingParameterWithNullName() {
+		NullPointerException e = assertThrows(NullPointerException.class, () -> Parameter.of(null, VALUE));
+
+		assertThat(e.getMessage(), equalTo("parameter name cannot be null"));
 	}
 
 	@Test
 	void shouldThrowExceptionWhenBuildingParameterWithBlankName() {
-		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> Parameter.of("   ", "value"));
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> Parameter.of(BLANK_STRING, VALUE));
 
-		assertThat(e.getMessage(), equalTo("Parameter name cannot be null or blank"));
+		assertThat(e.getMessage(), equalTo("Parameter name cannot be blank"));
 	}
 
 	@Test
 	void shouldBuildParameterString() {
-		Parameter parameter = Parameter.of("name", "value");
+		Parameter parameter = Parameter.of(NAME, VALUE);
 
 		String result = parameter.toString();
 
@@ -89,5 +133,36 @@ class ParameterTest {
 		String result = Parameter.value((Object[]) null);
 
 		assertThat(result, equalTo(null));
+	}
+
+	@Test
+	void shouldBuildValueFromObject() {
+		String parameter = Parameter.value(new A("test"));
+
+		assertThat(parameter, equalTo("test"));
+	}
+
+	@Test
+	void shouldBuildValueFromIntegerArray() {
+		String parameter = Parameter.value((Object) new Integer[] { 1, 2, 3 });
+
+		assertThat(parameter, equalTo("1,2,3"));
+	}
+
+	@Test
+	void shouldBuildValueFromIntArray() {
+		String parameter = Parameter.value(new int[] { 1, 2, 3 });
+
+		assertThat(parameter, equalTo("1,2,3"));
+	}
+
+	@Test
+	void shouldPutParameterIntoMap() {
+		Parameter parameter = Parameter.of(NAME, VALUE);
+		Map<String, String> map = new HashMap<>();
+
+		parameter.putInto(map);
+
+		assertThat(map.get(NAME), equalTo(VALUE));
 	}
 }
