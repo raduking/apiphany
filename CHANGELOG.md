25fadcb57ab1b6f7dfbafbbf84ab0148714c7936
raduking <radu.lazin@boatyardx.com>
2026-01-16 16:32:26 +0200

    Added missing formatter properties.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 - Upgraded `org.eclipse.jdt/org.eclipse.jdt.core` to `3.44.0`.
 - Added support for XML via `jackson-dataformat-xml` only available if library is provided.
 - Changed `Strings.fromFile` to be more deterministic, everything that starts with `/` is absolute, everything else relative.
+- Upgraded formatter version to `4.38`.
 
 ---
 
--- src/test/java/org/apiphany/formatter/FormatterTest.java
+++ src/test/java/org/apiphany/formatter/FormatterTest.java
@@ -1,9 +1,12 @@
 package org.apiphany.formatter;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.apiphany.lang.Strings;
 import org.eclipse.jdt.internal.formatter.DefaultCodeFormatterOptions;
@@ -13,6 +16,8 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 /**
@@ -26,17 +31,55 @@ public class FormatterTest {
 
 	private static final String FORMATTER_FILE_PATH = "java-formatter.xml";
 
-	@SuppressWarnings("unchecked")
 	@Test
 	void shouldHaveValidFormatterFile() throws JsonMappingException, JsonProcessingException {
 		String xml = Strings.fromFile(FORMATTER_FILE_PATH, e -> LOGGER.error("Could not read formatter file", e));
 
-		XmlMapper xmlMapper = new XmlMapper();
-		Map<String, String> optionsMap = xmlMapper.readValue(xml, HashMap.class);
+		Map<String, String> settings = getSettingsMap(xml);
 
 		DefaultCodeFormatterOptions options = DefaultCodeFormatterOptions.getDefaultSettings();
 
-		assertDoesNotThrow(() -> options.set(optionsMap));
+		assertDoesNotThrow(() -> options.set(settings));
+
+		for (Map.Entry<String, String> entry : options.getMap().entrySet()) {
+			String key = entry.getKey();
+			String expectedValue = entry.getValue();
+			String actualValue = settings.get(key);
+
+			if (!Objects.equals(actualValue, expectedValue)) {
+				LOGGER.info("Wrong/missing setting: '{}' : expected='{}' actual='{}'", key, expectedValue, actualValue);
+			}
+			assertThat("Setting '" + key + "' should match", actualValue, equalTo(expectedValue));
+		}
+	}
+
+	private static Map<String, String> getSettingsMap(final String xml) throws JsonProcessingException, JsonMappingException {
+		XmlMapper xmlMapper = new XmlMapper();
+		JsonNode xmlRoot = xmlMapper.readTree(xml);
+
+		Map<String, String> settings = new HashMap<>();
+
+		xmlRoot.get("profile")
+				.forEach(child -> {
+					if (!child.isArray()) {
+						return;
+					}
+					ArrayNode arrayNode = (ArrayNode) child;
+					arrayNode.forEach(setting -> {
+						JsonNode idNode = setting.get("id");
+						if (idNode == null) {
+							return;
+						}
+						JsonNode valueNode = setting.get("value");
+						if (valueNode == null) {
+							return;
+						}
+						String id = idNode.asText();
+						String value = valueNode.asText();
+						settings.put(id, value);
+					});
+				});
+		return settings;
 	}
 
 }
