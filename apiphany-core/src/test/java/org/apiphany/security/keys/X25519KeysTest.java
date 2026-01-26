9cfed855697f91884da853da61589c7426681886
raduking <radu.lazin@boatyardx.com>
2026-01-27 01:00:49 +0200

    More test coverage.

--- apiphany-core/src/test/java/org/apiphany/security/keys/X25519KeysTest.java
+++ apiphany-core/src/test/java/org/apiphany/security/keys/X25519KeysTest.java
@@ -3,6 +3,7 @@ package org.apiphany.security.keys;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.security.KeyPair;
@@ -79,5 +80,150 @@ class X25519KeysTest {
 
 		assertThat(publicKey, is(notNullValue()));
 		assertThat(publicKey, is(instanceOf(XECPublicKey.class)));
+	}
+
+	@Test
+	void shouldCorrectlyConvertFromLittleEndian() throws Exception {
+		byte[] littleEndianBytes = new byte[32];
+		littleEndianBytes[0] = 0x01;
+		littleEndianBytes[31] = 0x02;
+
+		PublicKey result = X25519Keys.INSTANCE.fromLittleEndian(littleEndianBytes);
+
+		assertThat(result, is(notNullValue()));
+		assertThat(result, is(instanceOf(XECPublicKey.class)));
+	}
+
+	@Test
+	void shouldCorrectlyConvertFromBigEndian() throws Exception {
+		byte[] bigEndianBytes = new byte[32];
+		bigEndianBytes[0] = 0x01;
+		bigEndianBytes[31] = 0x02;
+
+		PublicKey result = X25519Keys.INSTANCE.fromBigEndian(bigEndianBytes);
+
+		assertThat(result, is(notNullValue()));
+		assertThat(result, is(instanceOf(XECPublicKey.class)));
+	}
+
+	@Test
+	void shouldGenerateSharedSecretBetweenKeyPair() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] sharedSecret = X25519Keys.INSTANCE.getSharedSecret(keyPair.getPrivate(), keyPair.getPublic());
+
+		assertThat(sharedSecret, is(notNullValue()));
+		assertThat(sharedSecret.length, is(greaterThan(0)));
+	}
+
+	@Test
+	void shouldConvertPublicKeyToLittleEndianByteArray() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPublic(), BytesOrder.LITTLE_ENDIAN);
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldConvertPublicKeyToBigEndianByteArray() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPublic(), BytesOrder.BIG_ENDIAN);
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldCorrectlyConvertToBigEndianByteArrayInternally() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArrayBigEndian(keyPair.getPublic());
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldCorrectlyConvertToLittleEndianByteArrayInternally() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArrayLittleEndian(keyPair.getPublic());
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldConvertPrivateKeyToLittleEndianByteArray() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPrivate(), BytesOrder.LITTLE_ENDIAN);
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldConvertPrivateKeyToBigEndianByteArray() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPrivate(), BytesOrder.BIG_ENDIAN);
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldCorrectlyConvertPrivateToLittleEndianByteArrayInternally() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArrayLittleEndian(keyPair.getPrivate());
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldCorrectlyConvertPrivateToBigEndianByteArrayInternally() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+
+		byte[] bytes = X25519Keys.INSTANCE.toByteArrayBigEndian(keyPair.getPrivate());
+
+		assertThat(bytes, is(notNullValue()));
+		assertThat(bytes.length, is(32));
+	}
+
+	@Test
+	void shouldReturnTrueWhenKeysMatch() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+		byte[] publicKeyBytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPublic(), BytesOrder.BIG_ENDIAN);
+
+		boolean result = X25519Keys.INSTANCE.verifyKeyMatch(publicKeyBytes, BytesOrder.BIG_ENDIAN, keyPair.getPublic());
+
+		assertThat(result, is(true));
+	}
+
+	@Test
+	void shouldReturnFalseWhenKeysDontMatch() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+		byte[] differentBytes = new byte[32];
+		Arrays.fill(differentBytes, (byte) 0x99);
+
+		boolean result = X25519Keys.INSTANCE.verifyKeyMatch(differentBytes, BytesOrder.BIG_ENDIAN, keyPair.getPublic());
+
+		assertThat(result, is(false));
+	}
+
+	@Test
+	void shouldVerifyMatchWithLittleEndianByteOrder() {
+		KeyPair keyPair = X25519Keys.INSTANCE.generateKeyPair();
+		byte[] littleEndianBytes = X25519Keys.INSTANCE.toByteArray(keyPair.getPublic(), BytesOrder.LITTLE_ENDIAN);
+
+		boolean result = X25519Keys.INSTANCE.verifyKeyMatch(littleEndianBytes, BytesOrder.LITTLE_ENDIAN, keyPair.getPublic());
+
+		assertThat(result, is(true));
 	}
 }
