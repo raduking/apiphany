9c106d34cea129e1e7d0b4104bbe080007be5010
raduking <radu.lazin@boatyardx.com>
2026-01-28 20:45:10 +0200

    Add test summary to integration tests.

--- apiphany-core/src/main/java/org/apiphany/client/ClientCustomization.java
+++ apiphany-core/src/main/java/org/apiphany/client/ClientCustomization.java
@@ -8,7 +8,6 @@ package org.apiphany.client;
  * <li>{@link #DEFAULT}: Use the default customizations provided by the client implementation.</li>
  * <li>{@link #NONE}: No customization applied to the client.</li>
  * </ul>
- * </p>
  * Currently only these two options are supported and are only used in advanced scenarios.
  *
  * @author Radu Sebastian LAZIN
