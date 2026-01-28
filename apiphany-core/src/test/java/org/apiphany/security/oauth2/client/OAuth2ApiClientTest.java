d09c0b873142a8e80f2a145e39f627c6824a87cf
raduking <radu.lazin@boatyardx.com>
2026-01-28 03:24:20 +0200

    More unit test coverage for `OAuth2ApiClient`.

--- apiphany-core/src/test/java/org/apiphany/security/oauth2/client/OAuth2ApiClientTest.java
+++ apiphany-core/src/test/java/org/apiphany/security/oauth2/client/OAuth2ApiClientTest.java
@@ -1,5 +1,6 @@
 package org.apiphany.security.oauth2.client;
 
+import static org.apiphany.ParameterFunction.parameter;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,29 +10,41 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Map;
+
 import org.apiphany.ApiClient;
+import org.apiphany.ApiRequest;
 import org.apiphany.ApiResponse;
+import org.apiphany.RequestParameters;
 import org.apiphany.client.ExchangeClient;
 import org.apiphany.client.ExchangeClientBuilder;
 import org.apiphany.client.http.HttpExchangeClient;
 import org.apiphany.client.http.JavaNetHttpExchangeClient;
 import org.apiphany.header.Header;
+import org.apiphany.header.Headers;
 import org.apiphany.http.HttpAuthScheme;
 import org.apiphany.http.HttpException;
 import org.apiphany.http.HttpHeader;
+import org.apiphany.http.HttpMethod;
 import org.apiphany.http.HttpStatus;
+import org.apiphany.io.ContentType;
 import org.apiphany.json.JsonBuilder;
 import org.apiphany.lang.Strings;
 import org.apiphany.security.AuthenticationException;
 import org.apiphany.security.AuthenticationToken;
 import org.apiphany.security.AuthenticationType;
+import org.apiphany.security.JwsAlgorithm;
+import org.apiphany.security.keys.RSAKeys;
 import org.apiphany.security.oauth2.ClientAuthenticationMethod;
 import org.apiphany.security.oauth2.OAuth2ClientRegistration;
+import org.apiphany.security.oauth2.OAuth2Parameter;
 import org.apiphany.security.oauth2.OAuth2ProviderDetails;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -150,6 +163,159 @@ class OAuth2ApiClientTest {
 				() -> oAuth2ApiClient.getAuthenticationToken(ClientAuthenticationMethod.CLIENT_SECRET_BASIC));
 
 		assertThat(e.getCause(), equalTo(exception));
+	}
+
+	@Test
+	@SuppressWarnings({ "resource", "unchecked" })
+	void shouldReturnAuthenticationWithClientSecretBasic() {
+		doReturn(AuthenticationType.NONE).when(exchangeClient).getAuthenticationType();
+		doReturn(MAIN_EXCHANGE_CLIENT).when(exchangeClient).getName();
+		doReturn(HttpMethod.POST).when(exchangeClient).post();
+
+		AuthenticationToken expectedToken = new AuthenticationToken();
+		expectedToken.setExpiresIn(300);
+		ApiResponse<AuthenticationToken> apiResponse = ApiResponse.create(expectedToken).status(HttpStatus.OK).build();
+
+		ArgumentCaptor<ApiRequest<String>> requestCaptor = ArgumentCaptor.forClass(ApiRequest.class);
+		doReturn(apiResponse).when(exchangeClient).exchange(requestCaptor.capture());
+
+		oAuth2ApiClient = new OAuth2ApiClient(clientRegistration, providerDetails, exchangeClient);
+
+		AuthenticationToken token = oAuth2ApiClient.getAuthenticationToken(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+
+		assertThat(token, notNullValue());
+
+		ApiRequest<String> capturedRequest = requestCaptor.getValue();
+
+		Map<String, String> params = RequestParameters.of(
+				parameter(OAuth2Parameter.GRANT_TYPE, clientRegistration.getAuthorizationGrantType()),
+				parameter(OAuth2Parameter.EXPIRES_IN, OAuth2Parameter.Default.EXPIRES_IN.toSeconds()));
+
+		String body = RequestParameters.asString(RequestParameters.encode(params));
+		String expectedAuthHeader = clientRegistration.getAuthorizationHeaderValue(HttpAuthScheme.BASIC);
+		var headers = capturedRequest.getHeaders();
+
+		assertThat(capturedRequest.getBody(), equalTo(body));
+		assertThat(headers.size(), equalTo(2));
+		assertThat(Headers.get(HttpHeader.AUTHORIZATION, headers).getFirst(), equalTo(expectedAuthHeader));
+		assertThat(Headers.get(HttpHeader.CONTENT_TYPE, headers).getFirst(), equalTo(ContentType.Value.APPLICATION_FORM_URLENCODED));
+		assertThat(capturedRequest.getUrl().toString(), equalTo(providerDetails.getTokenUri()));
+		assertThat(capturedRequest.getMethod(), equalTo(HttpMethod.POST));
+	}
+
+	@Test
+	@SuppressWarnings({ "resource", "unchecked" })
+	void shouldReturnAuthenticationWithClientSecretPost() {
+		doReturn(AuthenticationType.NONE).when(exchangeClient).getAuthenticationType();
+		doReturn(MAIN_EXCHANGE_CLIENT).when(exchangeClient).getName();
+		doReturn(HttpMethod.POST).when(exchangeClient).post();
+
+		AuthenticationToken expectedToken = new AuthenticationToken();
+		expectedToken.setExpiresIn(300);
+		ApiResponse<AuthenticationToken> apiResponse = ApiResponse.create(expectedToken).status(HttpStatus.OK).build();
+
+		ArgumentCaptor<ApiRequest<String>> requestCaptor = ArgumentCaptor.forClass(ApiRequest.class);
+		doReturn(apiResponse).when(exchangeClient).exchange(requestCaptor.capture());
+
+		oAuth2ApiClient = new OAuth2ApiClient(clientRegistration, providerDetails, exchangeClient);
+
+		AuthenticationToken token = oAuth2ApiClient.getAuthenticationToken(ClientAuthenticationMethod.CLIENT_SECRET_POST);
+
+		assertThat(token, notNullValue());
+
+		ApiRequest<String> capturedRequest = requestCaptor.getValue();
+
+		Map<String, String> params = RequestParameters.of(
+				parameter(OAuth2Parameter.GRANT_TYPE, clientRegistration.getAuthorizationGrantType()),
+				parameter(OAuth2Parameter.EXPIRES_IN, OAuth2Parameter.Default.EXPIRES_IN.toSeconds()),
+				parameter(OAuth2Parameter.CLIENT_ID, clientRegistration.getClientId()),
+				parameter(OAuth2Parameter.CLIENT_SECRET, clientRegistration.getClientSecret()));
+
+		String body = RequestParameters.asString(RequestParameters.encode(params));
+		var headers = capturedRequest.getHeaders();
+
+		assertThat(capturedRequest.getBody(), equalTo(body));
+		assertThat(headers.size(), equalTo(1));
+		assertThat(Headers.get(HttpHeader.CONTENT_TYPE, headers).getFirst(), equalTo(ContentType.Value.APPLICATION_FORM_URLENCODED));
+		assertThat(capturedRequest.getUrl().toString(), equalTo(providerDetails.getTokenUri()));
+		assertThat(capturedRequest.getMethod(), equalTo(HttpMethod.POST));
+	}
+
+	@Test
+	@SuppressWarnings({ "resource", "unchecked" })
+	void shouldReturnAuthenticationWithClientSecretJwt() {
+		doReturn(AuthenticationType.NONE).when(exchangeClient).getAuthenticationType();
+		doReturn(MAIN_EXCHANGE_CLIENT).when(exchangeClient).getName();
+		doReturn(HttpMethod.POST).when(exchangeClient).post();
+
+		AuthenticationToken expectedToken = new AuthenticationToken();
+		expectedToken.setExpiresIn(300);
+		ApiResponse<AuthenticationToken> apiResponse = ApiResponse.create(expectedToken).status(HttpStatus.OK).build();
+
+		ArgumentCaptor<ApiRequest<String>> requestCaptor = ArgumentCaptor.forClass(ApiRequest.class);
+		doReturn(apiResponse).when(exchangeClient).exchange(requestCaptor.capture());
+
+		String clientRegistrationJsonString = Strings.fromFile("security/oauth2/oauth2-client-registration-jwt.json");
+		clientRegistration = JsonBuilder.fromJson(clientRegistrationJsonString, OAuth2ClientRegistration.class);
+
+		oAuth2ApiClient = new OAuth2ApiClient(clientRegistration, providerDetails, exchangeClient);
+
+		AuthenticationToken token = oAuth2ApiClient.getAuthenticationToken(ClientAuthenticationMethod.CLIENT_SECRET_JWT);
+
+		assertThat(token, notNullValue());
+
+		ApiRequest<String> capturedRequest = requestCaptor.getValue();
+
+		Map<String, String> bodyParams = RequestParameters.from(capturedRequest.getBody());
+		var headers = capturedRequest.getHeaders();
+
+		assertThat(bodyParams.get(OAuth2Parameter.GRANT_TYPE.value()), equalTo(clientRegistration.getAuthorizationGrantType().value()));
+		assertThat(bodyParams.get(OAuth2Parameter.CLIENT_ASSERTION_TYPE.value()), equalTo("urn:ietf:params:oauth:client-assertion-type:jwt-bearer"));
+		assertThat(bodyParams.get(OAuth2Parameter.EXPIRES_IN.value()), equalTo(String.valueOf(OAuth2Parameter.Default.EXPIRES_IN.toSeconds())));
+		assertThat(headers.size(), equalTo(1));
+		assertThat(Headers.get(HttpHeader.CONTENT_TYPE, headers).getFirst(), equalTo(ContentType.Value.APPLICATION_FORM_URLENCODED));
+		assertThat(capturedRequest.getUrl().toString(), equalTo(providerDetails.getTokenUri()));
+		assertThat(capturedRequest.getMethod(), equalTo(HttpMethod.POST));
+	}
+
+	@Test
+	@SuppressWarnings({ "resource", "unchecked" })
+	void shouldReturnAuthenticationWithClientSecretPK() {
+		doReturn(AuthenticationType.NONE).when(exchangeClient).getAuthenticationType();
+		doReturn(MAIN_EXCHANGE_CLIENT).when(exchangeClient).getName();
+		doReturn(HttpMethod.POST).when(exchangeClient).post();
+
+		AuthenticationToken expectedToken = new AuthenticationToken();
+		expectedToken.setExpiresIn(300);
+		ApiResponse<AuthenticationToken> apiResponse = ApiResponse.create(expectedToken).status(HttpStatus.OK).build();
+
+		ArgumentCaptor<ApiRequest<String>> requestCaptor = ArgumentCaptor.forClass(ApiRequest.class);
+		doReturn(apiResponse).when(exchangeClient).exchange(requestCaptor.capture());
+
+		String clientRegistrationJsonString = Strings.fromFile("security/oauth2/oauth2-client-registration-pk.json");
+		clientRegistration = JsonBuilder.fromJson(clientRegistrationJsonString, OAuth2ClientRegistration.class);
+
+		RSAPrivateKey privateKey = RSAKeys.loadPEMPrivateKey("security/oauth2/rsa_private.pem");
+		JwsAlgorithm jwsAlgorithm = JwsAlgorithm.fromString("RS256");
+
+		oAuth2ApiClient = new OAuth2ApiClient(clientRegistration, providerDetails, privateKey, jwsAlgorithm, exchangeClient);
+
+		AuthenticationToken token = oAuth2ApiClient.getAuthenticationToken(ClientAuthenticationMethod.PRIVATE_KEY_JWT);
+
+		assertThat(token, notNullValue());
+
+		ApiRequest<String> capturedRequest = requestCaptor.getValue();
+
+		Map<String, String> bodyParams = RequestParameters.from(capturedRequest.getBody());
+		var headers = capturedRequest.getHeaders();
+
+		assertThat(bodyParams.get(OAuth2Parameter.GRANT_TYPE.value()), equalTo(clientRegistration.getAuthorizationGrantType().value()));
+		assertThat(bodyParams.get(OAuth2Parameter.CLIENT_ASSERTION_TYPE.value()), equalTo("urn:ietf:params:oauth:client-assertion-type:jwt-bearer"));
+		assertThat(bodyParams.get(OAuth2Parameter.EXPIRES_IN.value()), equalTo(String.valueOf(OAuth2Parameter.Default.EXPIRES_IN.toSeconds())));
+		assertThat(headers.size(), equalTo(1));
+		assertThat(Headers.get(HttpHeader.CONTENT_TYPE, headers).getFirst(), equalTo(ContentType.Value.APPLICATION_FORM_URLENCODED));
+		assertThat(capturedRequest.getUrl().toString(), equalTo(providerDetails.getTokenUri()));
+		assertThat(capturedRequest.getMethod(), equalTo(HttpMethod.POST));
 	}
 
 	static class SimpleApiClient extends ApiClient {
