397039e175a4426978893f2f361e55e6ed836184
raduking <radu.lazin@boatyardx.com>
2026-01-28 11:49:35 +0200

    Tidy up the deployment.

--- maven_deploy.sh
+++ maven_deploy.sh
@@ -9,6 +9,10 @@ mvn clean verify -Dtest.tls.chunked=true
 # If we get here, all tests passed
 echo "All tests passed. Deploying apiphany module..."
 
+# -pl to specify the module to build (project list)
+# -am to also build any dependencies (also make)
+# -DskipTests to skip tests during deployment
+
 mvn deploy -Drelease=true \
 	-pl apiphany-core \
 	-am \
