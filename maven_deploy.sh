1493b851d6e19aa70a883f16c0dcb280051a3547
Radu Sebastian LAZIN <raduking@gmail.com>
2026-01-28 04:15:35 +0200

    Only run tests once.

--- maven_deploy.sh
+++ maven_deploy.sh
@@ -8,4 +8,10 @@ mvn clean verify -Dtest.tls.chunked=true
 
 # If we get here, all tests passed
 echo "All tests passed. Deploying apiphany module..."
-mvn deploy -Drelease=true -pl apiphany
+
+mvn deploy -Drelease=true \
+	-pl apiphany \
+	-am \
+	-DskipTests
+
+echo "Deployment completed successfully."
