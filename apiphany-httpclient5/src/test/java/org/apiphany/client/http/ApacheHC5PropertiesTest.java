da9b3ee73062d68dd7dbe5d597c7d4db06b1a353
raduking <radu.lazin@boatyardx.com>
2026-02-23 12:56:23 +0200

    Added `ApacheHC5Properties` coverage.

--- apiphany-httpclient5/src/test/java/org/apiphany/client/http/ApacheHC5PropertiesTest.java
+++ apiphany-httpclient5/src/test/java/org/apiphany/client/http/ApacheHC5PropertiesTest.java
@@ -3,10 +3,14 @@ package org.apiphany.client.http;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.ProtocolVersion;
 import org.apache.hc.core5.util.Timeout;
+import org.apiphany.json.JsonBuilder;
 import org.apiphany.json.jackson2.ApiphanyHC5Jackson2Module;
 import org.apiphany.json.jackson2.Jackson2JsonBuilder;
 import org.apiphany.lang.Strings;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
@@ -16,6 +20,19 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
  * Test class for {@link ApacheHC5Properties}.
  */
 class ApacheHC5PropertiesTest {
+
+	private static Jackson2JsonBuilder jsonBuilder;
+
+	@BeforeAll
+	static void setup() {
+		jsonBuilder = new Jackson2JsonBuilder(new YAMLFactory());
+		jsonBuilder.getObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.KEBAB_CASE);
+		jsonBuilder.getObjectMapper().registerModule(ApiphanyHC5Jackson2Module.instance());
+	}
+
+	private static <T> T fromYaml(final String yaml, final Class<T> valueType) {
+		return JsonBuilder.with(jsonBuilder, () -> JsonBuilder.fromJson(yaml, valueType));
+	}
 
 	static class ApplicationProperties {
 
@@ -35,18 +52,25 @@ class ApacheHC5PropertiesTest {
 	}
 
 	@Test
-	void shouldLoadPropertiesFromFile() {
+	void shouldLoadPropertiesFromFile() throws ParseException {
 		String filePath = "apache-hc5-properties.yaml";
 		String yaml = Strings.fromFile(filePath);
 
-		Jackson2JsonBuilder jsonBuilder = new Jackson2JsonBuilder(new YAMLFactory());
-		jsonBuilder.getObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.KEBAB_CASE);
-		jsonBuilder.getObjectMapper().registerModule(ApiphanyHC5Jackson2Module.instance());
-
-		ApplicationProperties properties = jsonBuilder.fromJsonString(yaml, ApplicationProperties.class);
+		ApplicationProperties properties = fromYaml(yaml, ApplicationProperties.class);
 
 		ApacheHC5Properties httpClient5Properties = properties.getHttpClient5();
 
 		assertThat(httpClient5Properties.getConnection().getTimeToLive(), equalTo(Timeout.ofSeconds(30)));
+		assertThat(httpClient5Properties.getConnection().getMaxTotal(), equalTo(100));
+		assertThat(httpClient5Properties.getConnection().getMaxPerRoute(), equalTo(50));
+
+		assertThat(httpClient5Properties.getRequest().getHttpProtocolVersion(), equalTo(ProtocolVersion.parse("HTTP/1.1")));
+		assertThat(httpClient5Properties.getRequest().isProtocolUpgradeEnabled(), equalTo(true));
+
+		assertThat(httpClient5Properties.getConnectionRequest().getTimeout(), equalTo(Timeout.INFINITE));
+
+		assertThat(httpClient5Properties.getConnect().getTimeout(), equalTo(Timeout.ofMinutes(1)));
+
+		assertThat(httpClient5Properties.getSocket().getTimeout(), equalTo(Timeout.ofHours(1)));
 	}
 }
