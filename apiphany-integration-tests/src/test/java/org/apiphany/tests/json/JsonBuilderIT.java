7d94585281cb7254c0f64168292b409b09eb0519
raduking <radu.lazin@boatyardx.com>
2026-03-04 19:08:33 +0200

    Added more JSON integration tests.

--- apiphany-integration-tests/src/test/java/org/apiphany/tests/json/JsonBuilderIT.java
+++ apiphany-integration-tests/src/test/java/org/apiphany/tests/json/JsonBuilderIT.java
@@ -7,6 +7,13 @@ import java.util.List;
 
 import org.apiphany.json.JsonBuilder;
 import org.apiphany.lang.Strings;
+import org.apiphany.lang.annotation.AsValue;
+import org.apiphany.lang.annotation.Creator;
+import org.apiphany.lang.annotation.FieldName;
+import org.apiphany.lang.annotation.FieldOrder;
+import org.apiphany.lang.annotation.Ignored;
+import org.apiphany.security.Sensitive;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.morphix.reflection.GenericClass;
 
@@ -23,17 +30,17 @@ class JsonBuilderIT {
 	private static final String ID = "id1";
 	private static final String NAME = "name1";
 
-	static class A {
+	static class SimpleDTO {
 
 		private String id;
 
 		private String name;
 
-		public A() {
+		public SimpleDTO() {
 			// empty
 		}
 
-		public A(final String id, final String name) {
+		public SimpleDTO(final String id, final String name) {
 			this.id = id;
 			this.name = name;
 		}
@@ -57,11 +64,11 @@ class JsonBuilderIT {
 
 	@Test
 	void shouldTransformObjectToJsonAndReadItBack() {
-		A a1 = new A(ID, NAME);
+		SimpleDTO a1 = new SimpleDTO(ID, NAME);
 
 		Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(a1));
 
-		A a2 = JsonBuilder.fromJson(json1, A.class);
+		SimpleDTO a2 = JsonBuilder.fromJson(json1, SimpleDTO.class);
 
 		Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(a2));
 
@@ -70,16 +77,355 @@ class JsonBuilderIT {
 
 	@Test
 	void shouldTransformGenericObjectToJsonAndReadItBack() {
-		List<A> list1 = List.of(new A(ID, NAME));
+		List<SimpleDTO> list1 = List.of(new SimpleDTO(ID, NAME));
 
 		Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(list1));
 
-		List<A> list2 = JsonBuilder.fromJson(json1, new GenericClass<>() {
+		List<SimpleDTO> list2 = JsonBuilder.fromJson(json1, new GenericClass<>() {
 			// empty
 		});
 
 		Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(list2));
 
 		assertThat(json1, equalTo(json2));
+	}
+
+	static class GenericDTO<T> {
+
+		private T value;
+
+		public GenericDTO() {
+			// empty
+		}
+
+		public GenericDTO(final T value) {
+			this.value = value;
+		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public void setValue(final T value) {
+			this.value = value;
+		}
+	}
+
+	@Test
+	void shouldTransformGenericObjectWithGenericFieldToJsonAndReadItBack() {
+		GenericDTO<SimpleDTO> a1 = new GenericDTO<>(new SimpleDTO(ID, NAME));
+
+		Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(a1));
+
+		GenericDTO<SimpleDTO> a2 = JsonBuilder.fromJson(json1, new GenericClass<>() {
+			// empty
+		});
+
+		Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(a2));
+
+		assertThat(json1, equalTo(json2));
+	}
+
+	@Test
+	void shouldTransformGenericObjectWithGenericFieldOfGenericTypeToJsonAndReadItBack() {
+		GenericDTO<List<SimpleDTO>> a1 = new GenericDTO<>(List.of(new SimpleDTO(ID, NAME)));
+
+		Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(a1));
+
+		GenericDTO<List<SimpleDTO>> a2 = JsonBuilder.fromJson(json1, new GenericClass<>() {
+			// empty
+		});
+
+		Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(a2));
+
+		assertThat(json1, equalTo(json2));
+	}
+
+	@Nested
+	class ApiphanyAnnotationTests {
+
+		static class ApiphanyDTO {
+
+			String id;
+
+			@Sensitive
+			String password;
+
+			public ApiphanyDTO() {
+				// empty
+			}
+
+			public ApiphanyDTO(final String id, final String password) {
+				this.id = id;
+				this.password = password;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(final String id) {
+				this.id = id;
+			}
+
+			public String getPassword() {
+				return password;
+			}
+
+			public void setPassword(final String password) {
+				this.password = password;
+			}
+		}
+
+		@Test
+		void shouldNotIncludeSensitiveFieldsInJson() {
+			ApiphanyDTO dto = new ApiphanyDTO(ID, "p123");
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"id\":\"id1\"}"));
+		}
+
+		static class NoDefaultConstructorDTO {
+
+			private String id;
+
+			@Creator
+			public NoDefaultConstructorDTO(final @FieldName("id") String id) {
+				this.id = id;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(final String id) {
+				this.id = id;
+			}
+		}
+
+		@Test
+		void shouldTransformObjectWithoutDefaultConstructorToJsonAndReadItBack() {
+			NoDefaultConstructorDTO a1 = new NoDefaultConstructorDTO(ID);
+
+			Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(a1));
+
+			NoDefaultConstructorDTO a2 = JsonBuilder.fromJson(json1, NoDefaultConstructorDTO.class);
+
+			Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(a2));
+
+			assertThat(json1, equalTo(json2));
+		}
+
+		static class CustomFieldNameDTO {
+
+			@FieldName("custom_id")
+			private String id;
+
+			public CustomFieldNameDTO() {
+				// empty
+			}
+
+			public CustomFieldNameDTO(final String id) {
+				this.id = id;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(final String id) {
+				this.id = id;
+			}
+		}
+
+		@Test
+		void shouldUseCustomFieldNameInJson() {
+			CustomFieldNameDTO dto = new CustomFieldNameDTO(ID);
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"custom_id\":\"id1\"}"));
+		}
+
+		static class CustomCreatorDTO {
+
+			private String id;
+
+			@Creator
+			public CustomCreatorDTO(final @FieldName("id") String id) {
+				this.id = id;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(final String id) {
+				this.id = id;
+			}
+		}
+
+		@Test
+		void shouldUseCustomCreatorToInstantiateObject() {
+			CustomCreatorDTO a1 = new CustomCreatorDTO(ID);
+
+			Object json1 = Strings.removeAllWhitespace(JsonBuilder.toJson(a1));
+
+			CustomCreatorDTO a2 = JsonBuilder.fromJson(json1, CustomCreatorDTO.class);
+
+			Object json2 = Strings.removeAllWhitespace(JsonBuilder.toJson(a2));
+
+			assertThat(json1, equalTo(json2));
+		}
+
+		static class IgnoredFieldDTO {
+
+			private String id;
+
+			@Ignored
+			private String ignoredField;
+
+			public IgnoredFieldDTO() {
+				// empty
+			}
+
+			public IgnoredFieldDTO(final String id, final String ignoredField) {
+				this.id = id;
+				this.ignoredField = ignoredField;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(final String id) {
+				this.id = id;
+			}
+
+			public String getIgnoredField() {
+				return ignoredField;
+			}
+
+			public void setIgnoredField(final String ignoredField) {
+				this.ignoredField = ignoredField;
+			}
+		}
+
+		@Test
+		void shouldNotIncludeIgnoredFieldsInJson() {
+			IgnoredFieldDTO dto = new IgnoredFieldDTO(ID, "ignored");
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"id\":\"id1\"}"));
+		}
+
+		enum CustomEnum {
+
+			VALUE1,
+			VALUE2;
+
+			@AsValue
+			@Override
+			public String toString() {
+				return this == VALUE1 ? "custom_value" : "VALUE2";
+			}
+		}
+
+		@Test
+		void shouldUseCustomFieldNameForEnumValue() {
+			CustomEnum e = CustomEnum.VALUE1;
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(e));
+
+			assertThat(json, equalTo("\"custom_value\""));
+		}
+
+		@Test
+		void shouldUseCustomToStringForEnumValue() {
+			CustomEnum e = CustomEnum.VALUE2;
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(e));
+
+			assertThat(json, equalTo("\"VALUE2\""));
+		}
+
+		static class EnumDTO {
+
+			private CustomEnum value;
+
+			public EnumDTO() {
+				// empty
+			}
+
+			public EnumDTO(final CustomEnum value) {
+				this.value = value;
+			}
+
+			public CustomEnum getValue() {
+				return value;
+			}
+
+			public void setValue(final CustomEnum value) {
+				this.value = value;
+			}
+		}
+
+		@Test
+		void shouldUseCustomToStringForEnumValueWhenNestedInObject() {
+			EnumDTO dto = new EnumDTO(CustomEnum.VALUE1);
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"value\":\"custom_value\"}"));
+		}
+
+		@Test
+		void shouldUseCustomToStringForEnumValueWhenNestedInObjectWithGenericType() {
+			GenericDTO<CustomEnum> dto = new GenericDTO<>(CustomEnum.VALUE2);
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"value\":\"VALUE2\"}"));
+		}
+
+		@FieldOrder({ "second", "first" })
+		static class FieldOrderDTO {
+
+			private String first;
+
+			private String second;
+
+			public FieldOrderDTO() {
+				// empty
+			}
+
+			public FieldOrderDTO(final String first, final String second) {
+				this.first = first;
+				this.second = second;
+			}
+
+			public String getFirst() {
+				return first;
+			}
+
+			public void setFirst(final String first) {
+				this.first = first;
+			}
+
+			public String getSecond() {
+				return second;
+			}
+		}
+
+		@Test
+		void shouldUseCustomFieldOrderInJson() {
+			FieldOrderDTO dto = new FieldOrderDTO("firstValue", "secondValue");
+
+			String json = Strings.removeAllWhitespace(JsonBuilder.toJson(dto));
+
+			assertThat(json, equalTo("{\"second\":\"secondValue\",\"first\":\"firstValue\"}"));
+		}
 	}
 }
