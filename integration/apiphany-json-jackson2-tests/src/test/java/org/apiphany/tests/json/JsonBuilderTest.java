de109a133a24893e89d633fd6c696aebbb3ac7ee
raduking <radu.lazin@boatyardx.com>
2026-03-04 19:08:33 +0200

    Added more JSON integration tests.

--- integration/apiphany-json-jackson2-tests/src/test/java/org/apiphany/tests/json/JsonBuilderTest.java
+++ integration/apiphany-json-jackson2-tests/src/test/java/org/apiphany/tests/json/JsonBuilderTest.java
@@ -1,7 +1,14 @@
 package org.apiphany.tests.json;
 
+import org.apiphany.json.JsonBuilder;
+
+/**
+ * Integration test class for Jackson 2 with {@link JsonBuilder}.
+ *
+ * @author Radu Sebastian LAZIN
+ */
 class JsonBuilderTest extends JsonBuilderIT {
 
-	// empty
+	// tests are inherited from JsonBuilderIT and must pass for every JSON library implementation
 
 }
