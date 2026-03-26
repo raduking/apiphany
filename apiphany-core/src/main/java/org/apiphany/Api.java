13ab50d567728c3b59ea1d1c4b33eee3526a10b0
raduking <radu.lazin@boatyardx.com>
2026-03-26 18:48:45 +0200

    More test coverage.

--- apiphany-core/src/main/java/org/apiphany/Api.java
+++ apiphany-core/src/main/java/org/apiphany/Api.java
@@ -1,15 +1,19 @@
 package org.apiphany;
 
 import org.apiphany.client.ClientLifecycle;
+import org.apiphany.client.ExchangeClient;
 import org.apiphany.client.ExchangeClientBuilder;
 import org.apiphany.client.http.HttpClientFluentAdapter;
+import org.apiphany.lang.Require;
+import org.morphix.lang.JavaArrays;
 
 /**
  * API interface for the Apiphany library. This interface provides static factory methods for creating API clients with
  * different configurations. It serves as the entry point for ephemeral API client creation, allowing users to easily
- * obtain clients for making HTTP requests with fluent syntax. The API clients created through this interface are
- * designed to be short-lived and are suitable for scenarios where clients are used for single operations or sessions
- * and then discarded.
+ * obtain clients for making HTTP requests with fluent syntax.
+ * <p>
+ * The API clients created through this interface are designed to be short-lived and are suitable for scenarios where
+ * clients are used for single operations or sessions and then discarded.
  *
  * @author Radu Sebastian LAZIN
  */
@@ -24,6 +28,10 @@ public interface Api {
 	 */
 	@SuppressWarnings("resource")
 	public static HttpClientFluentAdapter http(final ExchangeClientBuilder... exchangeClientBuilders) {
+		if (JavaArrays.isNotEmpty(exchangeClientBuilders)) {
+			Require.that(exchangeClientBuilders.length == 1, "Only one {} is allowed for ephemeral API clients.",
+					ExchangeClient.class.getName());
+		}
 		ApiClient apiClient = ApiClient.of(exchangeClientBuilders);
 		apiClient.setLifecycle(ClientLifecycle.EPHEMERAL);
 		return apiClient.http();
