a9f9bdae8fc9d1b01cb72878e6297f431a941e4a
Radu Sebastian LAZIN <raduking@gmail.com>
2026-04-16 02:38:16 +0300

    Added `ExecutionWrapper` support to `OAuth2TokenProvider`.

--- apiphany-core/src/test/java/org/apiphany/security/oauth2/OAuth2TokenProviderSpecTest.java
+++ apiphany-core/src/test/java/org/apiphany/security/oauth2/OAuth2TokenProviderSpecTest.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Instant;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Supplier;
 
+import org.apiphany.security.AuthenticationTokenProvider;
 import org.junit.jupiter.api.Test;
+import org.morphix.lang.function.ExecutionWrapper;
 
 /**
  * Test class for {@link OAuth2TokenProviderSpec}.
@@ -19,44 +22,71 @@ import org.junit.jupiter.api.Test;
  */
 class OAuth2TokenProviderSpecTest {
 
+	private static final String TEST_REGISTRATION = "test-registration";
+	private static final String TEST_PROVIDER = "test-provider";
+
 	@Test
 	@SuppressWarnings("resource")
 	void shouldCreateWithDefaults() throws Exception {
-		OAuth2TokenProviderSpec config = OAuth2TokenProviderSpec.builder().build();
+		OAuth2TokenProviderSpec spec = OAuth2TokenProviderSpec.builder().build();
 
-		assertNotNull(config.getTokenProviderProperties());
-		assertNotNull(config.getTokenRefreshScheduler());
-		assertNotNull(config.getTokenClientSupplier());
-		assertNotNull(config.getDefaultExpirationSupplier());
-		assertTrue(config.getTokenRefreshScheduler().isManaged());
+		OAuth2TokenClientSupplier defaultTokenClientSupplier = spec.getTokenClientSupplier();
+		ExecutionWrapper<Void> defaultUpdateTokenWrapper = spec.getUpdateTokenWrapper();
 
-		config.getTokenRefreshScheduler().closeIfManaged();
+		try {
+			assertNotNull(spec.getTokenProviderProperties());
+			assertNotNull(spec.getTokenRefreshScheduler());
+			assertNotNull(defaultTokenClientSupplier);
+			assertNotNull(spec.getDefaultExpirationSupplier());
+			assertEquals(ExecutionWrapper.EMPTY, defaultUpdateTokenWrapper);
+
+			AuthenticationTokenProvider tokenProvider = defaultTokenClientSupplier.get(null, null);
+
+			assertNull(tokenProvider);
+
+			assertTrue(spec.getTokenRefreshScheduler().isManaged());
+		} finally {
+			spec.getTokenRefreshScheduler().closeIfManaged();
+		}
 	}
 
 	@Test
 	@SuppressWarnings("resource")
 	void shouldCreateWithCustomValues() {
 		OAuth2TokenProviderProperties properties = new OAuth2TokenProviderProperties();
-		OAuth2ResolvedRegistration registration = OAuth2ResolvedRegistration.of(
-				"test-registration",
-				new OAuth2ClientRegistration(),
-				new OAuth2ProviderDetails());
+
+		OAuth2Properties oauth2properties = new OAuth2Properties();
+
+		OAuth2ClientRegistration clientRegistration = new OAuth2ClientRegistration();
+		clientRegistration.setClientId("test-client-id");
+		clientRegistration.setClientSecret("test-client-secret");
+		clientRegistration.setProvider(TEST_PROVIDER);
+
+		OAuth2ProviderDetails providerDetails = new OAuth2ProviderDetails();
+
+		oauth2properties.setRegistration(Map.of(TEST_REGISTRATION, clientRegistration));
+		oauth2properties.setProvider(Map.of(TEST_PROVIDER, providerDetails));
+
 		OAuth2TokenClientSupplier tokenClientSupplier = (cr, pd) -> null;
 		var defaultExpirationSupplier = (Supplier<Instant>) Instant::now;
+		ExecutionWrapper<Void> wrapper = s -> null;
 
 		try (ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor()) {
 			OAuth2TokenProviderSpec config = OAuth2TokenProviderSpec.builder()
 					.properties(properties)
-					.registration(registration)
+					.registration(oauth2properties)
 					.tokenRefreshScheduler(scheduler)
 					.tokenClientSupplier(tokenClientSupplier)
 					.defaultExpirationSupplier(defaultExpirationSupplier)
+					.updateTokenWrapper(wrapper)
 					.build();
 
 			assertEquals(properties, config.getTokenProviderProperties());
 			assertEquals(scheduler, config.getTokenRefreshScheduler().unwrap());
 			assertEquals(tokenClientSupplier, config.getTokenClientSupplier());
 			assertEquals(defaultExpirationSupplier, config.getDefaultExpirationSupplier());
+			assertEquals(wrapper, config.getUpdateTokenWrapper());
+			assertNotNull(config.getResolvedRegistration());
 		}
 	}
 
