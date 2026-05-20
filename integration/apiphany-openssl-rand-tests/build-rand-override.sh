4592ed60065f90bf8fcf5b83f12013a1f0c3aa74
raduking <radu.lazin@boatyardx.com>
2026-05-20 10:55:57 +0300

    Do not delete openssl clone if already exists.

--- integration/apiphany-openssl-rand-tests/build-rand-override.sh
+++ integration/apiphany-openssl-rand-tests/build-rand-override.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p build
+
 gcc -dynamiclib -o build/openssl_rand_override_mac.dylib src/openssl_rand_override_mac.c \
 	-I$HOME/local/openssl-nonhardened/include \
 	-L$HOME/local/openssl-nonhardened/lib \
